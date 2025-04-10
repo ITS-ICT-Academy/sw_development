79fd00c27ede8a1b18331a55a107cc6a56dc7f4d
Toni Mancini <tmancini@di.uniroma1.it>
2025-04-10 09:28:25 +0200

    Release

--- dev/install_python_packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /environment
-pip install -r "${REQUIREMENTS_FILE}"
--- sample_user_base_folder/simple_test/test.py
+++ sample_user_base_folder/simple_test/test.py
@@ -2,8 +2,9 @@ import os
 import psycopg
 
 if __name__ == '__main__':
-	print(f"Questo programma è eseguito all'interno del container nella directory {os.getcwd()}.")
-	print("\nTest della connessione a PostgreSQL:")
+	print(f"Questo programma è eseguito all'interno del container")
+	print(f"La directory corrente è {os.getcwd()}.")
+	print("Test della connessione a PostgreSQL:")
 
 	with psycopg.connect("host=postgresql dbname=postgres user=postgres password=postgres") as conn:
 		# Open a cursor to perform database operations
