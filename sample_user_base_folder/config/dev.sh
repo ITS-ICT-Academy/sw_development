169e3d533a5973216c0612a7c292625fabdeb322
Toni Mancini <tmancini@di.uniroma1.it>
2025-07-17 23:58:26 +0200

    Docs improved

--- sample_user_base_folder/config/dev.sh
+++ sample_user_base_folder/config/dev.sh
@@ -4,7 +4,7 @@ date
 echo "Executing script '${0} to configure 'dev' container"
 echo "Working directory: $(pwd)"
 
-##### Python configurations #####
+##### Python configuration #####
 PYTHON_REQUIREMENTS="./python_requirements.txt"
 echo "Installing python packages from file $(realpath "${PYTHON_REQUIREMENTS}")"
 pip install -r "./python_requirements.txt"
@@ -13,17 +13,9 @@ pip install -r "./python_requirements.txt"
 ##### NodeJS & React #####
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 22
-
-# Verify the Node.js version:
-node -v # Should print "v22.17.0".
+\. "$HOME/.nvm/nvm.sh" # in lieu of restarting the shell
+nvm install 22 # Download and install Node.js:
+node -v # Print Node.js version: should print "v22.17.0".
 nvm current # Should print "v22.17.0".
-
-# Verify npm version:
-npm -v # Should print "10.9.2".
+npm -v # Print npm version: should print "10.9.2".
 
